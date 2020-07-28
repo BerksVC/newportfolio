f8c69275c0533f7e2e883e2fe01962bedf66c150
BerksVC <kirbyveracruz@outlook.com>
2020-07-28 11:33:50 +0800

    My First Commit

---
+++ dist/js/main.js
@@ -0,0 +1,42 @@
+const menuBtn = document.querySelector(".menu-btn")
+const burger = document.querySelector(".menu-btn_burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav_item");
+const homeNameFirst = document.querySelector(".home_name-first");
+const homeNameLast = document.querySelector(".home_name-last");
+const aboutMenu = document.querySelector('#aboutmenu')
+const expMenu = document.querySelector('#expmenu')
+const contactMenu = document.querySelector('#contactmenu')
+const projMenu = document.querySelector('#projmenu')
+//const menuLink = document.querySelector("#aboutbtn");
+
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    burger.classList.toggle("open");
+    nav.classList.toggle("open");
+    menuNav.classList.toggle("open");
+    homeNameFirst.classList.toggle("open");
+    homeNameLast.classList.toggle("open");
+    //console.log("Fuck you");
+    navItems.forEach(item => item.classList.toggle('open'));
+}
+
+navItems.forEach(item => item.addEventListener("click", toggleNav));
+
+function toggleNav(){
+    burger.classList.toggle("open");
+    nav.classList.toggle("open");
+    menuNav.classList.toggle("open");
+    homeNameFirst.classList.toggle("open");
+    homeNameLast.classList.toggle("open");
+    navItems.forEach(item => item.classList.toggle('open'));
+    aboutMenu.classList.toggle("active");
+    expMenu.classList.toggle("active");
+    contactMenu.classList.toggle("active");
+    projMenu.classList.toggle("active");
+    //navItems.forEach(item => item.classList.toggle('active'));
+    //console.log("Fuck you");
+}

06ebb667d0b067d49d4da323d10db8252194c0ba
BerksVC <kirbyveracruz@outlook.com>
2020-07-29 14:45:49 +0800

    Project Area
    - Background color
    - added image for project
    - mobile responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,8 @@ const aboutMenu = document.querySelector('#aboutmenu')
 const expMenu = document.querySelector('#expmenu')
 const contactMenu = document.querySelector('#contactmenu')
 const projMenu = document.querySelector('#projmenu')
+const projWork = document.querySelectorAll('.proj_work')
+const projItem = document.querySelectorAll('.proj_item')
 //const menuLink = document.querySelector("#aboutbtn");
 
 
@@ -33,10 +35,19 @@ function toggleNav(){
     homeNameFirst.classList.toggle("open");
     homeNameLast.classList.toggle("open");
     navItems.forEach(item => item.classList.toggle('open'));
-    aboutMenu.classList.toggle("active");
-    expMenu.classList.toggle("active");
-    contactMenu.classList.toggle("active");
-    projMenu.classList.toggle("active");
+    // aboutMenu.classList.toggle("active");
+    // expMenu.classList.toggle("active");
+    // contactMenu.classList.toggle("active");
+    // projMenu.classList.toggle("active");
     //navItems.forEach(item => item.classList.toggle('active'));
-    //console.log("Fuck you");
+    console.log("Fuck you");
 }
+
+projWork.forEach(item => item.addEventListener("click", toggleProj));
+
+function toggleProj(){
+    projItem.forEach(item => item.classList.toggle('open'));
+    console.log("Fuck you");
+}
+
+

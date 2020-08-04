b89eb1c1dbd5afe7d14e669422c44510febed514
BerksVC <kirbyveracruz@outlook.com>
2020-08-04 23:05:24 +0800

    fixed toggle of projects
    - added parameters to catch the clicked div

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,13 +5,14 @@ const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".menu-nav_item");
 const homeNameFirst = document.querySelector(".home_name-first");
 const homeNameLast = document.querySelector(".home_name-last");
-const aboutMenu = document.querySelector('#aboutmenu')
-const expMenu = document.querySelector('#expmenu')
-const contactMenu = document.querySelector('#contactmenu')
-const projMenu = document.querySelector('#projmenu')
-const projWork = document.querySelectorAll('.proj_work')
-const projItem = document.querySelectorAll('.proj_item')
+// const aboutMenu = document.querySelector('#aboutmenu')
+// const expMenu = document.querySelector('#expmenu')
+// const contactMenu = document.querySelector('#contactmenu')
+// const projMenu = document.querySelector('#projmenu')
+//const projWork = document.querySelectorAll('.proj_work');
+const projItem = document.querySelectorAll('.proj_item');
 //const menuLink = document.querySelector("#aboutbtn");
+
 
 
 menuBtn.addEventListener("click", toggleMenu);
@@ -40,14 +41,40 @@ function toggleNav(){
     // contactMenu.classList.toggle("active");
     // projMenu.classList.toggle("active");
     //navItems.forEach(item => item.classList.toggle('active'));
-    console.log("Fuck you");
+    //console.log("Fuck you");
 }
 
-projWork.forEach(item => item.addEventListener("click", toggleProj));
-
-function toggleProj(){
-    projItem.forEach(item => item.classList.toggle('open'));
-    console.log("Fuck you");
+//projItem.forEach(item => item.addEventListener("click", toggleProj));
+projItem.forEach(selectorDiv);
+function selectorDiv(proj){
+    const projSelect = proj.querySelector('.proj_select');
+    projSelect.addEventListener("click", toggleProj);
+    function toggleProj(){
+        projItem.forEach(function(item){
+            if (item !== proj){
+                item.classList.remove("open");
+            }
+        })
+        proj.classList.toggle("open");
+        //console.log("It fucking works")
+    }
 }
+// function toggleProj(){
+//     projItem.classList.toggle("open");
+//     console.log("Fuck you");
+// }
+// projItem.forEach(function(question){
+//     //const btn = question.querySelector(".question-btn");
+    
+//     projSelect.addEventListener("click", function(){
+//     // questions.forEach(function(item){
+//     //     if (item !== question){
+//     //         item.classList.remove("show-text");
+//     //     }
+//     // })   
+//         question.classList.toggle("open");
+//         console.log("It didn't work")
+//     });
+// });
 
 
